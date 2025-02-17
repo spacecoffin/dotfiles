4aa7a56fde1dd110040d4601cf800de0ce4a383e
Reed Rosenberg <spacecoffin@users.noreply.github.com>
2025-02-17 17:46:30 -0500

    hs: add config & spoons to external

---
+++ home/dot_hammerspoon/init.lua
@@ -0,0 +1,145 @@
+hs.loadSpoon('EmmyLua')
+
+----------------------------------------------------------------------------------------
+-- sqlfluff
+----------------------------------------------------------------------------------------
+
+SQLFluffPath = "$HOME/.local/bin/sqlfluff"
+
+function MakeArgument(argument, value)
+    if value then
+        return "--" .. argument .. " " .. value
+    end
+    return ""
+end
+
+function MakeFmtFnString(dialect, templater, excludeRules)
+    dialect = MakeArgument("dialect", dialect)
+    templater = MakeArgument("templater", templater)
+    excludeRules = MakeArgument("exclude-rules", excludeRules)
+
+    local formatString = [[
+        function fmt {
+            cd $HOME
+            local SQLFILE=$HOME/.hssqlfluff.sql
+            pbpaste > $SQLFILE
+            %s fix %s %s %s --ignore=templating $SQLFILE > $HOME/.hssqlfluff.log
+            cat $SQLFILE | pbcopy
+        }
+        fmt
+    ]]
+    return string.format(formatString, SQLFluffPath, dialect, templater, excludeRules)
+end
+
+function MakePressedFn(dialect, templater, excludeRules)
+    return function()
+        hs.eventtap.keyStroke({ "cmd" }, "c")
+        local output, status = hs.execute(MakeFmtFnString(dialect, templater, excludeRules), false)
+
+        if status then
+            hs.eventtap.keyStroke({ "cmd" }, "v")
+        else
+            hs.alert('Attempting to format with sqlfluff failed.')
+        end
+    end
+end
+
+-- Fix BigQuery
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "F", nil, MakePressedFn("bigquery", "jinja"))
+
+-- Fix MySQL
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "M", nil, MakePressedFn("mysql", "placeholder")) -- "python"))
+
+-- Fix SparkSQL
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "S", nil, MakePressedFn("sparksql", "python"))
+
+-- Lint Bigquery
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "B", nil, function()
+    hs.eventtap.keyStroke({ "cmd" }, "c")
+    local output, status = hs.execute([[
+        function lnt {
+            cd $HOME
+            local SQLFILE=$HOME/.hssqlfluff.sql
+            pbpaste > $SQLFILE
+            $HOME/.local/bin/sqlfluff lint --dialect bigquery --templater jinja $SQLFILE > $HOME/.hssqlfluff.log
+        }
+        lnt
+    ]], false)
+end)
+
+-- Lint MySQL
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "N", nil, function()
+    hs.eventtap.keyStroke({ "cmd" }, "c")
+    local output, status = hs.execute([[
+        function lnt {
+            cd $HOME
+            local SQLFILE=$HOME/.hssqlfluff.sql
+            pbpaste > $SQLFILE
+            $HOME/.local/bin/sqlfluff lint --dialect mysql --templater jinja $SQLFILE > $HOME/.hssqlfluff.log
+        }
+        lnt
+    ]], false)
+end)
+
+
+----------------------------------------------------------------------------------------
+-- Sublime Text style Upper Case text
+----------------------------------------------------------------------------------------
+
+-- https://github.com/babarrett/hammerspoon/blob/master/editSelection.lua
+
+-- getTextSelection()
+--  Gets currently selected text using Cmd+C
+--  Saves and restores the current pasteboard
+--  Imperfect, perhaps. May not work well on large selections.
+--  Taken from: https://github.com/Hammerspoon/hammerspoon/issues/634
+function GetTextSelection() -- returns text or nil while leaving pasteboard undisturbed.
+    local oldText = hs.pasteboard.getContents()
+    hs.eventtap.keyStroke({ "cmd" }, "c")
+    hs.timer.usleep(25000)
+    local text = hs.pasteboard.getContents() -- if nothing is selected this is unchanged
+    hs.pasteboard.setContents(oldText)
+    if text ~= oldText then
+        return text
+    else
+        return ""
+    end
+end
+
+function ToUppercase()
+    local sel = GetTextSelection()
+    if sel then hs.eventtap.keyStrokes(string.upper(sel)) end
+end
+
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "U", nil, ToUppercase)
+--hs.uielement:selectedText().upper()
+
+function ToLowercase()
+    local sel = GetTextSelection()
+    if sel then hs.eventtap.keyStrokes(string.lower(sel)) end
+end
+
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "L", nil, ToLowercase)
+
+
+----------------------------------------------------------------------------------------
+-- BigQuery labels
+----------------------------------------------------------------------------------------
+
+function GetDefaultBQLabels()
+    hs.eventtap.keyStrokes([[SET @@query_label = "product:feature-store";
+SET @@query_label = "team:ds";
+SET @@query_label = "executor:rrosenberg";
+
+]])
+end
+
+hs.hotkey.bind({ "alt", "cmd", "ctrl", "shift" }, "Q", nil, GetDefaultBQLabels)
+
+
+----------------------------------------------------------------------------------------
+-- Spoons
+----------------------------------------------------------------------------------------
+
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
